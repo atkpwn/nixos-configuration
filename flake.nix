5b51247c04513781731b1020b55798b9ced79014
Attakorn Putwattana <atkpwn@gmail.com>
2024-07-29 01:22:50 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "My NixOS flake";
+
+  # references:
+  # https://github.com/Misterio77/nix-starter-configs
+  # https://nixos.wiki/wiki/Flakes
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware }:
+    let
+      system = "x86_64-linux";
+    in {
+
+      nixosConfigurations = {
+        myNixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit system; };
+
+          modules = [
+            ./nixos/configuration.nix
+            nixos-hardware.nixosModules.lenovo-thinkpad-x270
+          ];
+        };
+      };
+
+    };
+}
